6cdfbb79c2f1030aa5447f5b2863419442e94f70
Long Vu <lvu@guidewire.com>
2022-08-02 15:20:51 -0400

    added main app

--- src/MainApp.js
+++ src/MainApp.js
@@ -2,9 +2,17 @@
 
 import React from "react";
 import Container from "@mui/material/Container";
+import { Table, Typography } from "@mui/material";
 
 const MainApp = (props) => {
-  return <Container maxWidth="lg"></Container>;
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h3" component="h2" textAlign="center">
+        Welcome to Time off Calculator
+      </Typography>
+      <Table></Table>
+    </Container>
+  );
 };
 
 export default MainApp;
